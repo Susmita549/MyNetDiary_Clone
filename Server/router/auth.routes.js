0e2706506e28e11fe2cbfb952fe7514d29b6546d
Sranjan4321 <shashi94ranjan@gmail.com>
2022-07-20 21:24:45 +0530

    commit day-2

--- Server/router/auth.routes.js
+++ Server/router/auth.routes.js
@@ -2,17 +2,25 @@ const express = require('express');
 let authRouter = express.Router();
 const UserModel = require('../model/user');
 authRouter.post('/signup', async (req, res) => {
-  const result = await new UserModel(req.body);
-  result.save((err, sucess) => {
-    if (err) {
-      res.status(500).send({ message: 'Error occurred' });
-    } else {
-      res.status(201).send({ message: 'signup success' });
-    }
-  });
+  // console.log(req.body.email);
+  const result = await UserModel.find({ email: req.body.email });
+  !result.length
+    ? await new UserModel(req.body).save((err, sucess) => {
+        if (err) {
+          res.status(500).send({ message: 'Error occurred' });
+        } else {
+          res.status(201).send({ message: 'signup success' });
+        }
+      })
+    : res.status(201).send({ message: 'email already exhist' });
 });
-authRouter.post('/login', (req, res) => {
-  res.send('login users');
+authRouter.post('/login', async (req, res) => {
+  const result = await UserModel.find(req.body);
+  if (result.length > 0) {
+    res.send(result);
+  } else {
+    res.status(201).send({ message: 'Wrong credentials' });
+  }
 });
 
 module.exports = authRouter;
