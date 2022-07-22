cf0b306249e1c432213d2359459cf30a5e6cc080
rameshmane7218 <rameshmane7218@gmail.com>
2022-07-22 11:05:34 +0530

    made responsive footer

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -43,7 +43,7 @@ function App() {
 
         </Routes>
         {/* <Dashboard/> */}
-        {/* <Footer /> */}
+        <Footer />
       </Box>
     </ChakraProvider>
   );
--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -29,42 +29,70 @@ const Footer = () => {
         p="5rem 1rem 0 1rem"
         // border={'1px solid blue'}
       >
-        <Flex justifyContent={'space-between'}>
-          <Box mr={'1.5rem'}>
-            <Box mb={'3.5rem'}>
+        <Box mb={'2.5rem'} display={['block', 'block', 'block', 'none']}>
+          <Link to="/">
+            <Image src={footerItem.logo} h={['42px', '42px', '68px']} />
+          </Link>
+        </Box>
+        <Flex
+          justifyContent={['center', 'center', 'center', 'space-between']}
+          flexDirection={[
+            'column-reverse',
+            'column-reverse',
+            'column-reverse',
+            'row',
+          ]}
+        >
+          <Box mr={['0', '1.5rem']}>
+            <Box mb={'3.5rem'} display={['none', 'none', 'none', 'block']}>
               <Link to="/">
-                <Image src={footerItem.logo} h={'68px'} />
+                <Image src={footerItem.logo} h={['65px','65px','65px','63px','68px']} />
               </Link>
             </Box>
-            <Flex gap={4}>
-              {footerItem.store.map(store => (
-                <Box key={uuidv4()} as="a" target="_blank" href={store.href}>
-                  <Image src={store.icon} width="144px" />
-                </Box>
-              ))}
-            </Flex>
-            <Flex color={'#5b6067'} mt="1rem" gap={1}>
-              {footerItem.stars.map(star => (
-                <Image key={uuidv4()} src={star} width="24px" h={'24px'} />
-              ))}
-              <Text as={'strong'}>4.8</Text>
-              <Text as={'span'} color="#beaeac" position={'relative'}>
-                <Text
-                  position={'absolute'}
-                  top="-1"
-                  m="0 3px"
-                  fontSize={'40px'}
-                  lineHeight="0.3"
-                >
-                  {'.'}
+            <Flex
+              gap={4}
+              fontSize={['lg', 'xl']}
+              flexDirection={['column', 'column', 'row', 'column']}
+            >
+              <Flex gap={4}>
+                {footerItem.store.map(store => (
+                  <Box key={uuidv4()} as="a" target="_blank" href={store.href}>
+                    <Image src={store.icon} width="144px" />
+                  </Box>
+                ))}
+              </Flex>
+              <Flex color={'#5b6067'} mt="1rem" gap={1}>
+                {footerItem.stars.map(star => (
+                  <Image
+                    key={uuidv4()}
+                    src={star}
+                    width={['20px', '24px']}
+                    h={['20px', '24px']}
+                  />
+                ))}
+                <Text as={'strong'}>4.8</Text>
+                <Text as={'span'} color="#beaeac" position={'relative'}>
+                  <Text
+                    position={'absolute'}
+                    top="-1"
+                    m="0 3px"
+                    fontSize={'40px'}
+                    lineHeight="0.3"
+                  >
+                    {'.'}
+                  </Text>
                 </Text>
-              </Text>
-              <Text fontWeight={500} ml="0.7rem">
-                61,383 Ratings
-              </Text>
+                <Text fontWeight={500} ml="0.7rem">
+                  61,383 Ratings
+                </Text>
+              </Flex>
             </Flex>
             <UnorderedList listStyleType={'none'} ml="0">
-              <Flex m="45px 0 10px" alignItems={'center'} gap={2}>
+              <Flex
+                m={['15px 0 10px', '45px 0 10px']}
+                alignItems={'center'}
+                gap={2}
+              >
                 {footerItem.socialMedia.map(item => (
                   <ListItem
                     key={uuidv4()}
@@ -86,8 +114,8 @@ const Footer = () => {
                       // background="rgba(110,0,0,.11)"
                     >
                       <Flex
-                        w={'56px'}
-                        h="56px"
+                        w={['50px', '56px']}
+                        h={['50px', '56px']}
                         border="2px solid rgba(110,0,0,.11)"
                         rounded={'50%'}
                         justifyContent="center"
@@ -106,16 +134,24 @@ const Footer = () => {
             </UnorderedList>
           </Box>
           <Box>
-            <Grid templateColumns="repeat(4, auto)" gap={5}>
+            <Grid
+              templateColumns={[
+                'repeat(2, 1fr)',
+                'repeat(2, 1fr)',
+                'repeat(4, auto)',
+              ]}
+              gridTemplateAreas={'revert'}
+              gap={[2, 3, 5]}
+            >
               {footerItem.links.map(link => (
                 <Box key={uuidv4()}>
                   <Heading
                     as="h6"
-                    fontSize={'20px'}
+                    fontSize={['18px', '20px']}
                     fontWeight={500}
                     color={'#46618a'}
                     textAlign="left"
-                    m="1rem 0 3rem 0"
+                    m={['0.5rem 0 2rem 0','0.5rem 0 2rem 0', '1rem 0 3rem 0']}
                   >
                     {link.heading}
                   </Heading>
@@ -127,10 +163,10 @@ const Footer = () => {
                         listStyleType={'none'}
                         position="relative"
                         pl="1rem"
-                        mb="1rem"
+                        mb={['0.5rem', '0.5rem', '1rem']}
                         color={'#46618a'}
                         fontWeight={500}
-                        fontSize={'20px'}
+                        fontSize={['sm', 'md', 'md', '19px']}
                         _before={{
                           position: 'absolute',
                           top: '-10',
@@ -158,20 +194,31 @@ const Footer = () => {
         width={'100%'}
       >
         <Box>
-          <Box maxW={'1470px'} m="auto" p="0 15px">
+          <Box maxW={'1470px'} m="auto" p={['0 5px', '0 15px']}>
             <Flex
-              justifyContent={'space-between'}
+              justifyContent={['center', 'space-between']}
               fontWeight={500}
-              fontSize={'lg'}
+              fontSize={['sm', 'lg']}
+              flexDirection={['column-reverse','column-reverse','column-reverse', 'row']}
             >
-              <Flex gap={2} color="#46618a">
+              <Flex
+                gap={[1, 2]}
+                color="#46618a"
+                flexDirection={['column','column','row', 'row']}
+                justifyContent={'center'}
+              >
                 <Text>Copyright © 2022 MyNetDiary Inc.</Text>
                 <Text>All rights reserved.</Text>
               </Flex>
 
-              <Flex alignItems={'center'} color="#46618a">
+              <Flex
+                alignItems={'center'}
+                color="#46618a"
+                justifyContent={['center', '']}
+                mb={['10px', '']}
+              >
                 <Text
-                  p="0 15px"
+                  p={['0 10px', '0 15px']}
                   _hover={{
                     textDecoration: 'underline',
                     cursor: 'pointer',
@@ -179,9 +226,12 @@ const Footer = () => {
                 >
                   Contact Us
                 </Text>
-                <Box h={'0.8rem'} borderLeft={'1px solid #ccbebc'} />
+                <Box
+                  h={['0.5rem', '0.8rem']}
+                  borderLeft={'1px solid #ccbebc'}
+                />
                 <Text
-                  p="0 15px"
+                  p={['0 10px', '0 15px']}
                   _hover={{
                     textDecoration: 'underline',
                     cursor: 'pointer',
@@ -189,9 +239,12 @@ const Footer = () => {
                 >
                   FAQ
                 </Text>
-                <Box h={'0.8rem'} borderLeft={'1px solid #ccbebc'} />
+                <Box
+                  h={['0.5rem', '0.8rem']}
+                  borderLeft={'1px solid #ccbebc'}
+                />
                 <Text
-                  p="0 15px"
+                  p={['0 10px', '0 15px']}
                   _hover={{
                     textDecoration: 'underline',
                     cursor: 'pointer',
@@ -199,9 +252,12 @@ const Footer = () => {
                 >
                   Privacy
                 </Text>
-                <Box h={'0.8rem'} borderLeft={'1px solid #ccbebc'} />
+                <Box
+                  h={['0.5rem', '0.8rem']}
+                  borderLeft={'1px solid #ccbebc'}
+                />
                 <Text
-                  p="0 15px"
+                  p={['0 10px', '0 15px']}
                   _hover={{
                     textDecoration: 'underline',
                     cursor: 'pointer',
