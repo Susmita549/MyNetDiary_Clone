647c53c15aec1e73ae2d3b2f07803fc5ab72a6cc
rameshmane7218 <rameshmane7218@gmail.com>
2022-07-24 11:40:36 +0530

    updatate

--- Frontend/src/pages/Library.jsx
+++ Frontend/src/pages/Library.jsx
@@ -2,11 +2,13 @@ import { Box } from '@chakra-ui/react';
 import React from 'react';
 import Diets from './Diets';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 const Library = () => {
   return (
     <Box>
       <Navbar />
       <Diets />
+      <Footer />
     </Box>
   );
 };
